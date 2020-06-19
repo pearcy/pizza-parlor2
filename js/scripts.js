6466cd1de2f50d79e385774715192b88df2d6aee
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-19 08:58:03 -0700

    add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,26 @@ PizzaOrder.prototype.addOrder = function(order) {
 // User Interface Logic
 
 
+$(document).ready(function() {
+  
+  $("form#pizzaOrder").submit(function(event) {
+    event.preventDefault();
 
+    let pizzaOrder = new PizzaOrder();
+    let inputtedSize = $("input[Type='radio']:checked").val();
+    // console.log(inputtedSize);
+
+    let inputtedTop1 = $("input[type=checkbox][name=top1]:checked").val();
+    let inputtedTop2 = $("input[type=checkbox][name=top2]:checked").val();
+    let inputtedTop3 = $("input[type=checkbox][name=top3]:checked").val();
+    let inputtedTop4 = $("input[type=checkbox][name=top4]:checked").val();
+
+
+   
+
+    
+
+  });
+  
+});
 
